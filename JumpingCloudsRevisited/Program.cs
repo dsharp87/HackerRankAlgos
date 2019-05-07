fdac37917482b5fdb8a62e4a634d6107f47a7609
Daniel Swayne (HCL America Inc) <v-daswa@microsoft.com>
2019-05-07 11:07:25 -0700

    adding two more

---
+++ JumpingCloudsRevisited/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace JumpingCloudsRevisited
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] c = new int[]{0,0,1,0,0,1,1,0};
+            int k = 2;
+            jumpingOnClouds(c, k);
+        }
+
+        static int jumpingOnClouds(int[] c, int k) {
+            int e = 100;
+            for(int i = 0; i < c.Length; i += k)
+            {
+                if(c[(i + k) % c.Length] == 1)
+                {
+                    e -= 2;
+                }
+                e -= 1;
+            }
+            return e;
+        }
+
+    }
+}
---
+++ Sequence_Equation/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Sequence_Equation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+
+            static int[] permutationEquation(int[] p) {
+                //itterate from 1 - x
+                //for each iterations, check value that returns iterator
+                //then check value that returns that value
+
+                int[] result = new int[p.Length];
+                for(int i = 1; i <= p.Length; i++)
+                {
+                    int x = Array.IndexOf(p, i) + 1;
+                    int y = Array.IndexOf(p, x) + 1;
+                    result[i -1] = y;
+                }
+                return result;
+            }
+    }
+}
