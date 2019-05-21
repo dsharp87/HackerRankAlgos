08b373135ea5fdc0875a321b24baf35fbe339cc9
Daniel Swayne (HCL America Inc) <v-daswa@microsoft.com>
2019-05-21 14:47:42 -0700

    adding sherlockSquares

--- Sherlock_and_Squares/Program.cs
+++ Sherlock_and_Squares/Program.cs
@@ -6,15 +6,47 @@ namespace Sherlock_and_Squares
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int a = 12;
+            int b = 179;
+            squares(a,b);
         }
 
         static int squares(int a, int b) 
         {
-
-            return 1;
+            //first lets find the next square
+            int nextSquare = 0;
+            for(int i = a; i <= b; i++)
+            {
+                if(Math.Sqrt(i) % 1 == 0)
+                {
+                    nextSquare = i;
+                    break;
+                }
+            }
+            if(nextSquare == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                int squareCount = 0;
+                int currentRoot = (int)Math.Sqrt(nextSquare);
+                while(nextSquare <= b)
+                {
+                    squareCount++;
+                    nextSquare = nextSquare + 2*currentRoot + 1;
+                    currentRoot++;
+                }
+                return squareCount;   
+            }
         }
     }
+
+
+
+
+
+
 
     //azure sql stack, what can it do asside from just using sql queries
 
