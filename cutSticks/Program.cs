b5c172dee7b40bc29502179e1298820d97739ac1
Daniel Swayne (HCL America Inc) <v-daswa@microsoft.com>
2019-05-21 16:49:44 -0700

    adding stickcut

---
+++ cutSticks/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace cutSticks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int[] sticks = new int[]{5,4,4,2,2,8};
+            int[] result = cutTheSticks(sticks);
+            System.Console.WriteLine("done");
+        }
+
+            static int[] cutTheSticks(int[] arr) 
+            {
+                List<int> arrToLst = arr.ToList();
+                List<int> stickCount = new List<int>{};
+                while(arrToLst.Count > 0)
+                {
+                    stickCount.Add(arrToLst.Count);
+                    int cutAmount = arrToLst.Min();
+                    for(int i = 0; i < arrToLst.Count; i++)
+                    {
+                        if(arrToLst[i] - cutAmount <= 0)
+                        {
+                            arrToLst.RemoveAt(i);
+                            i--;
+                        }
+                        else
+                        {
+                            arrToLst[i] -= cutAmount;
+                        }
+                    }
+                }
+                int[] result = stickCount.ToArray();
+                return result;
+            }
+
+    }
+}
