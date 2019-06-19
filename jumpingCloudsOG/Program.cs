9a9f071ed1e2a97b747c1d3c04adea06d28ec2da
Daniel Swayne (HCL America Inc) <v-daswa@microsoft.com>
2019-06-19 12:21:22 -0700

    adding jumpingClouds and repeatedstring

---
+++ jumpingCloudsOG/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace jumpingCloudsOG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int[] c = new int[]{0,0,1,0,0,1,0};
+            jumpingOnClouds(c);
+        }
+
+        static int jumpingOnClouds(int[] c) {
+            int jumpCount = 0;
+            int pos = 0;
+            while(pos < c.Length - 1)
+            {
+                if(pos + 2 < c.Length)
+                {
+                    if(c[pos + 2] == 0)
+                    {
+                        pos+= 2;
+                    }
+                    else
+                    {
+                        pos++;
+                    }
+                }
+                else
+                {
+                    pos++;
+                }
+                jumpCount++;
+            }
+            return jumpCount;
+        }
+    }
+}
---
+++ repeatString/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace repeatString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static long repeatedString(string s, long n) 
+        {
+            //count the number of a's in a in string s, 
+            int aCountInString = 0;
+            foreach(char c in s)
+            {
+                if(c == 'a')
+                {
+                    aCountInString++;
+                }
+            }
+            long remaining = n % s.Length;
+            int extraA = 0;
+            for(int i = 0; i < remaining; i++)
+            {
+                if(s[i] == 'a')
+                {
+                    extraA++;
+                }
+            }
+            long totalCount = n/s.Length*aCountInString + extraA;
+            return totalCount;
+        }
+    }
+}
