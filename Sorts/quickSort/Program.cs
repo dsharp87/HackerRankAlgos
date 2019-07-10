0da6683b333d9b68bd57e75eaecfb6eb1cc3bcc6
Daniel Swayne (HCL America Inc) <v-daswa@microsoft.com>
2019-07-09 18:58:19 -0700

    adding quickSort

---
+++ Sorts/quickSort/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace quickSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int[] test = new int[]{22,4,17,12,17,9,50,27,7,19};
+            test = QSort(test, 0, test.Length - 1);
+        }
+
+        public static int[] QSort(int[] arr, int low, int high)
+        {   
+            if(low < high)
+            {
+                int p = partition(arr, low, high);
+                QSort(arr, low, p - 1);
+                QSort(arr, p + 1, high);
+            }
+            return arr;
+        }
+
+        public static int partition(int[] arr, int low, int high)
+        {
+            int pivot = arr[high];
+            int s = low - 1;
+            for(int i = low; i < high; i++)
+            {
+                if(arr[i] <= pivot)
+                {
+                    s++;
+                    int temp = arr[s];
+                    arr[s] = arr[i];
+                    arr[i] = temp;
+                }
+            }
+            arr[high] = arr[s+1];
+            arr[s+1] = pivot;
+            return s+1;
+        }
+
+
+    }
+}
