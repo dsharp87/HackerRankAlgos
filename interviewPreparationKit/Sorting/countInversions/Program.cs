050231648d9a704a3e3cea94068c8cde9e8775c9
dswayne599 <dswayne599@gmail.com>
2019-08-19 20:53:30 -0700

    adding first string algo

---
+++ interviewPreparationKit/Sorting/countInversions/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace countInversions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+
+        static long countInversions(int[] arr) {
+            long swaps = 0;
+            bool hasSwapped = true;
+            while (hasSwapped)
+            {
+                hasSwapped = false;
+                for(int i = 0; i < arr.Length - 1; i++)
+                {
+                    
+                    if(arr[i] > arr[i+1])
+                    {
+                        int temp = arr[i];
+                        arr[i] = arr[i+1];
+                        arr[i + 1] = temp;
+                        swaps++;
+                        hasSwapped = true;
+                    }
+                }
+            }
+            return swaps;
+        }
+    }
+}
---
+++ interviewPreparationKit/Strings/makeingAnagrams/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace makeingAnagrams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string a = "showman";
+            string b = "woman";
+            int answer = makeAnagram(a,b);
+            System.Console.WriteLine(answer);
+        }
+
+        static int makeAnagram(string a, string b) {
+
+            //need to inventory each string
+            //iterate through each string and count difference between each letter
+            int i = 0;
+            int result = 0;
+            int minLength = a.Length >= b.Length ? b.Length : a.Length;
+            int[] aVals = new int[26];
+            int[] bVals = new int[26];
+            while(i < minLength)
+            {
+                aVals[char.ToUpper(a[i]) - 65]++;
+                bVals[char.ToUpper(b[i]) - 65]++;
+                i++;
+            }
+            if(minLength != b.Length)
+            {
+                for(int j = i; j < b.Length; j++)
+                    bVals[char.ToUpper(b[j]) - 65]++;
+            }
+            else
+            {
+                for(int j = i; j < a.Length; j++)
+                    aVals[char.ToUpper(a[j]) - 65]++;
+            }
+            for(int idx = 0; idx < 26; idx++)
+            {
+                if(aVals[idx] > bVals[idx])
+                    result += aVals[idx] - bVals[idx];
+                else
+                    result += bVals[idx] - aVals[idx];
+            }
+            return result;
+        }
+    }
+}
